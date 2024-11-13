cb87a80762df3ee2c9793ddcb7cf6822c697f66d
Teodora Cvetkovic <teodra.cvetkovic@elfak.rs>
2024-11-13 18:27:13 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./Home.js";
+import "./index.css";
+
+const root = createRoot(document.body);
+root.render(<Home />);
