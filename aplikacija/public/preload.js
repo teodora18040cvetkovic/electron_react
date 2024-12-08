852bdc55de01c79abbeb293d246d34772ea94396
Teodora Cvetkovic <teodra.cvetkovic@elfak.rs>
2024-12-09 00:37:11 +0100

    final

--- aplikacija/public/preload.js
+++ aplikacija/public/preload.js
@@ -1,5 +1,5 @@
-const { contextBridge } = require("electron");
+// const { contextBridge } = require("electron");
 
-contextBridge.exposeInMainWorld("electron", {
-  doSomething: () => console.log("This is a function exposed to React"),
-});
+// contextBridge.exposeInMainWorld("electron", {
+//   doSomething: () => console.log("This is a function exposed to React"),
+// });
